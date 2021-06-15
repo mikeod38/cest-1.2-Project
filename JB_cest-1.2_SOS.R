f17b466b3f5869753287fb85ee70c3b71eed86bd
juliabalch <julia.balch@gmail.com>
2021-06-15 15:31:36 -0400

    new data analysis

--- JB_cest-1.2_SOS.R
+++ JB_cest-1.2_SOS.R
@@ -1,18 +1,35 @@
 library(tidyverse)
 library(ggbeeswarm)
 theme_set(theme_classic())
-N2 <-c(4.21,7.28,2.02,10.46,20.15,7.12,5.46,1.78,2.95,4.17,2.88,3.45,3.06,11.94,1.68,12.02,2.26,3.99,5.78,2.69)
-cest1.2 <- c(2.45,2.03,9.97,9.73,15.15,17.95,3.60,2.96,3.49,4.71,2.92,2.31,6.01,3.90,14.53,1.50,8.87,2.83,6.52,3.65)
 
-tibble(genotype = rep(c("N2", "cest1.2"), each = 20), 
-       time = c(N2,cest1.2)) %>%
+#initial dataset
+
+N2_a <-c(4.21,7.28,2.02,10.46,20.15,7.12,5.46,1.78,2.95,4.17,2.88,3.45,3.06,11.94,1.68,12.02,2.26,3.99,5.78,2.69)
+cest1.2_a <- c(2.45,2.03,9.97,9.73,15.15,17.95,3.60,2.96,3.49,4.71,2.92,2.31,6.01,3.90,14.53,1.50,8.87,2.83,6.52,3.65)
+
+tibble(genotype = rep(c("N2_a", "cest1.2_a"), each = 20), 
+       time = c(N2_a,cest1.2_a)) %>%
   ggplot(aes(x = genotype, y = time)) +
   geom_boxplot(aes(fill = genotype), alpha = 0.5) +
   geom_quasirandom(width = 0.1) +
   #scale_fill_brewer(palette = "PuOr")
   scale_fill_manual(values = c("darkorchid3", "darkorange2"))
 
-mean(N2)
-mean(cest1.2)
+mean(N2_a)
+mean(cest1.2_a)
 
-t.test(N2, cest1.2)
+#repeated data
+
+N2_b <- c(6.39,7.02,4.23,8.59,3.14,11.44,5.88,2.71,2.83,6.49,9.89,5.09,9.18,7.56,2.03,13.18,1.05,7.27,20.01,9.92)
+cest1.2_b <- c(5.18,8.02,12.32,3.88,3.76,9.17,11.95,11.52,5.49,2.60,5.77,6.43,5.03,2.19,6.39,3.43,8.83,3.85,5.54,15.79)
+
+tibble(genotype = rep(c("N2_b", "cest1.2_b"), each = 20), 
+       time = c(N2_b,cest1.2_b)) %>%
+  ggplot(aes(x = genotype, y = time)) +
+  geom_boxplot(aes(fill = genotype), alpha = 0.5) +
+  geom_quasirandom(width = 0.1) +
+  #scale_fill_brewer(palette = "PuOr")
+  scale_fill_manual(values = c("darkorchid3", "darkorange2"))
+
+mean(N2_b)
+mean(cest1.2_b)
--- anterior_touch_cest-1.2.R
+++ anterior_touch_cest-1.2.R
@@ -4,15 +4,17 @@ library(ggbeeswarm)
 #initial data 
 
 AntTouch <- read_csv('cest-1.2 anterior touch practice.csv')
+AntTouch1 <- read_csv("cest-1.2_anterior_touch_repeat.csv")
 
-
-bodybendPlot <- AntTouch %>%
+bodybendPlot1 <- AntTouch %>%
   #filter(NumBends < 10) %>%
   ggplot(aes(x = genotype, y = NumBends)) +
   geom_boxplot() +
   stat_summary(geom = "errorbar", fun.data = "mean_se", width = 0.25, color = "red") +
   ggbeeswarm::geom_quasirandom(width = .25) +
   facet_grid(.~Time_off_food)
+
+bodybendPlot1
 #Are num bends dependent on time on plate? 
 (bends_by_order <- AntTouch %>%
     group_by(genotype, Time_off_food) %>%
@@ -21,3 +23,12 @@ bodybendPlot <- AntTouch %>%
     geom_point()) +
   facet_grid(Time_off_food ~ genotype, scales = "free_y") +
   geom_smooth(method = "lm")
+
+#recent data
+bodybendPlot2 <- AntTouch1 %>%
+  ggplot(aes(x = genotype, y = bends)) +
+  geom_boxplot() +
+  stat_summary(geom = "errorbar", fun.data = "mean_se", width = 0.25, color = "red") +
+  ggbeeswarm::geom_quasirandom(width = .25) +
+  facet_grid(.~observer)
+bodybendPlot2  
